35280ec486b94be2a5b90d84ed0447edc4420acd
John Levi <juguilon.levi.3@gmail.com>
2019-03-15 15:07:53 +0800

    refactored markup, style, css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function() {
     // automatically sets the year
-    document.querySelector("#myFooter").innerHTML = "(c) " + new Date(Date.now()).getFullYear() + " johhnnydev";
-})();
+    document.querySelector("#myFooter").innerHTML = "(c) " + new Date(Date.now()).getFullYear() + " - johhnnydev";
+ });
