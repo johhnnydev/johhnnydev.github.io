9d003af486467095ea1bfb793be211ee300d5208
John Levi <juguilon.levi.3@gmail.com>
2019-03-15 19:22:45 +0800

    tufte

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     // automatically sets the year
-    document.querySelector("#myFooter").innerHTML = "(c) " + new Date(Date.now()).getFullYear() + " - johhnnydev";
+    document.querySelector("#myFooter").innerHTML = "(c) " + new Date(Date.now()).getFullYear() + " - johhnnydev | style isn't mine";
  });
