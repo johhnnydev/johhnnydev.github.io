50fef00fc664b4da97bf51438cb36f14a4e3037f
John Levi <juguilon.levi.3@gmail.com>
2019-09-01 10:20:49 +0800

    refresh

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // automatically sets the year
-    document.querySelector("#myFooter").innerHTML = "(c) " + new Date(Date.now()).getFullYear() + " - johhnnydev | style isn't mine";
- });
