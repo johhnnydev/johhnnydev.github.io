fa0739525d243c1092e50d183bc2a6b9dcdf1bfa
dotjl <juguilon.levi.3@gmail.com>
2020-03-28 14:12:21 +0800

    - added gather all my repo - modified the layout

---
+++ script.js
@@ -0,0 +1,32 @@
+(function() {
+  fetch("https://api.github.com/users/johhnnydev/repos")
+    .then(res => res.json())
+    .then(res => {
+      let index = 0;
+      res.forEach(repo => {
+        // console.log(repo.name, repo.url);
+        let repoDiv = document.createElement("div");
+        repoDiv.classList.add("repo");
+
+        let repoIndex = document.createElement("span");
+        repoIndex.innerText = index;
+        index++;
+        repoIndex.classList.add("repo-index");
+
+        let repoUrl = document.createElement("anchor");
+        repoUrl.setAttribute("href", repo.url);
+        repotUrl.innerText = repo.name.toLowerCase();
+        repoUrl.classList.add("repo-url");
+
+        let repoDescription = document.createElement("p");
+        repoDescription.innerText = repo.description;
+        repoDescription.classList.add("repo-description");
+
+        repoDiv.appendChild(repoIndex);
+        repoDiv.appendChild(repoUrl);
+        repoDiv.appendChild(repoDescription);
+
+        document.querySelector("#repo-container").appendChild(repoDiv);
+      });
+    });
+})();
