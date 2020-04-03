af229c51ec47d47589e66b11f185d4f9e2443f2c
dotjl <juguilon.levi.3@gmail.com>
2020-04-03 10:52:25 +0800

    utf8 json text encoding

--- getAllMyGitHubRepos.ps1
+++ getAllMyGitHubRepos.ps1
@@ -1,6 +1,6 @@
 $myGitHubUrl = "https://api.github.com/users/johhnnydev/repos"
 
-(Invoke-RestMethod  -URI $myGitHubUrl) | Select-Object name, html_url, description, created_at | ConvertTo-Json | Out-File .\myrepos.json
+(Invoke-RestMethod  -URI $myGitHubUrl) | Select-Object name, html_url, description, created_at | ConvertTo-Json | Out-File ".\myrepos.json" -Encoding "UTF8"
 
 
 
